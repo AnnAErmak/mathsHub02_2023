0290a7fd09d4b049503bd77ed311bd8010f5dc83
Ermak Anna <ermak-anna86@yandex.ru>
2023-02-24 14:44:57 +0300

    add about_us

--- script.js
+++
@@ -1,8 +0,0 @@
-const skill = document.getElementById("skill");
-const isLove = document.getElementById("isLove");
-
-const skillText = prompt("Какой язык вы учите?", "пока не в курсе");
-const isLoveValue = confirm("Вы любите изучаемый язык");
-
-skill.innerText = skillText;
-isLove.innerText = isLoveValue;
